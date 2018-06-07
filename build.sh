032972adc6db81f986959026745750d9f7cea46f
Albert <me@albert.sh>
2018-06-08 01:06:42 +0200

    Add build instructions for website stuff

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 cat libs/lineWrapDetector.js main.js > extension/contentScript.js
+cp extension/contentScript.js docs/contentScript.js
 
 while getopts ":i" opt; do
   case $opt in
     i)
       convert promo/wasp.png -resize 16x16 extension/icons/wasp16.png
+      cp extension/icons/wasp16.png docs/favicon.png
       convert promo/wasp.png -resize 24x24 extension/icons/wasp24.png
       convert promo/wasp.png -resize 32x32 extension/icons/wasp32.png
       convert promo/wasp.png -resize 48x48 extension/icons/wasp48.png
