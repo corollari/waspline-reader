3cd3c35b7919a53ce11ad72a9130127fabce99b0
InvisibleUp <invisibleup@outlook.com>
2019-06-19 00:31:01 -0400

    Cleanup main.js to be more flexible
    - Put defaults in consts
    - Generalized getGradient into a lerp applied to all 3 RGB channels
    - Made names nicer, used Linux kernel-like coding style
    - Replaced hardcoded colors with a list of colors in hex format that is
      iterated through
    - Wrapped code in a function so that consts only get initialized once
    - Switched to custom branch of lineWrapDetector to fix bug when function
      is ran more than once
    - Froze docs/contentScript.js

--- main.js
+++ main.js
@@ -1,37 +1,70 @@
-function applyGradient(GRADIENT_LENGTH_PERCENTAGE){
-	var ps = document.getElementsByTagName('p');
-	for(let i=0; i<ps.length; i++){
-		var p = ps[i];
-		var lines = lineWrapDetector.getLines(p);
-		for(let j=0; j<lines.length; j++){
-			let line = lines[j];
-			let red = 0, blue = 0, green = 0;
-			for(let k=0; k<line.length; k++){
-				switch(j%4){
-					case 0: //Blue ending
-						blue = k>(line.length-1)*(1-GRADIENT_LENGTH_PERCENTAGE) ? getGradient(k, line.length) : 0;
-						break;
-					case 1: //Blue beginning
-						blue = k<(line.length-1)*GRADIENT_LENGTH_PERCENTAGE ? getGradient(k, line.length, true) : 0;
-						break;
-					case 2: //Red ending
-						red = k>(line.length-1)*(1-GRADIENT_LENGTH_PERCENTAGE) ? getGradient(k, line.length) : 0;
-						break;
-					case 3: //Red beginning
-						red = k<(line.length-1)*GRADIENT_LENGTH_PERCENTAGE ? getGradient(k, line.length, true) : 0;
-						break;
-				}
-				line[k].style.color="rgb("+red+","+green+","+blue+")";
+(function() {
+'use strict';
+const DEFAULT_GRADIENT_LENGTH = 0.5;
+const DEFAULT_GRADIENT_COLORS = ["#FF0000", "#0000FF"];
+const DEFAULT_TEXT_COLOR = "#000000";
+
+// Linear interpolate between v0 and v1 at percent t
+function lerp(v0, v1, t)
+{
+	return v0 * (1 - t) + v1 * t
+}
+
+// Convert a hex triplet (#XXXXXX) to an array containing red, green, and blue
+function hex_to_rgb(hex)
+{
+	return hex.replace('#', '').match(/.{1,2}/g).map(
+		x => parseInt(x, 16)
+	);
+}
+
+// Color all lines in the page
+function applyGradient(colors, color_text, gradient_size)
+{
+	const paragraphs = document.getElementsByTagName('p');
+	const base_color = hex_to_rgb(color_text);
+
+	for (let paragraph of paragraphs) {
+		const lines = lineWrapDetector.getLines(paragraph);
+
+		for (let lineno in lines) {
+			const line = lines[lineno];
+
+			// Alternate between left and right for every color
+			const active_color = hex_to_rgb(colors[
+				Math.floor((lineno % (colors.length * 2)) / 2)
+			]);
+			const is_left = (lineno % 2 === 0);
+
+			for (let loc in line) {
+				let t = Math.min((gradient_size / 100) + (loc / line.length), 1)
+				if (!is_left) { t = 1 - t; }
+
+				const red = lerp(base_color[0], active_color[0], t);
+				const green = lerp(base_color[1], active_color[1], t);
+				const blue = lerp(base_color[2], active_color[2], t);
+
+				line[loc].style.color = "rgb(" + (red|0) + "," + (green|0) + "," + (blue|0) + ")";
 			}
 		}
 	}
-	function getGradient(k, len, beginning){
-		return Math.round(Math.abs(255*((beginning?1:0)-(k-(beginning?0:(len-1)*(1-GRADIENT_LENGTH_PERCENTAGE)))/((len-1)*GRADIENT_LENGTH_PERCENTAGE))));
-	}
 }
 
-try{
-	chrome.storage.local.get(["GRADIENT_LENGTH_PERCENTAGE"], (result)=>applyGradient(result["GRADIENT_LENGTH_PERCENTAGE"]||0.5));
+try {
+	chrome.storage.local.get(
+		["GRADIENT_LENGTH_PERCENTAGE"],
+		(result) => applyGradient(
+			DEFAULT_GRADIENT_COLORS,
+			DEFAULT_TEXT_COLOR,
+			result["GRADIENT_LENGTH_PERCENTAGE"] || DEFAULT_GRADIENT_LENGTH,
+		)
+	);
 } catch(e){
-	applyGradient(0.5);
+	applyGradient(
+		DEFAULT_GRADIENT_COLORS,
+		DEFAULT_TEXT_COLOR,
+		DEFAULT_GRADIENT_LENGTH
+	);
 }
+
+})();
